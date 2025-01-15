687e543bb0bdaa778c0bc9f6dd3c543f8813d77a
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-01-15 21:58:27 +0000

    removed the code for the github calendar for now.

--- front-page.php
+++ front-page.php
@@ -8,38 +8,4 @@
 	<?php get_template_part('includes/section', 'content');?>
 </div>
 
-<!-- Include the library. -->
-<script
-  src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js">
-</script>
-
-<!-- Optionally, include the theme (if you don't want to struggle to write the CSS) -->
-<link
-  rel="stylesheet"
-  href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
-/>
-
-<!-- Prepare a container for your calendar. -->
-<div class="calendar">
-    <!-- Loading stuff -->
-    Loading the data just for you.
-</div>
-
-<script>
-    GitHubCalendar(".calendar", "your-username");
-
-    // or enable responsive functionality:
-    //GitHubCalendar(".calendar", "your-username", { responsive: true });
-
-    // Use a proxy
-    GitHubCalendar(".calendar", "helloxonie", {
-       proxy (username) {
-         return fetch(`https://your-proxy.com/github?user=${helloxonie}`)
-       }
-    }).then(r => r.text())
-</script>
-
-
-
-
 <?php get_footer();?>
