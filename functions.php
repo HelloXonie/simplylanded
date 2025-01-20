a7969c00d3abc0f992959c6be88f8684afdfe3e2
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-01-20 07:42:00 +0000

    adding functionality for menus

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+//Load Stylesheets
 function load_css()
 {
 	wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(),
@@ -17,7 +17,7 @@ function load_css()
 
 add_action('wp_enqueue_scripts', 'load_css');
 
-
+//Load Javascript
 function load_js()
 {
 	
@@ -30,3 +30,7 @@ function load_js()
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
+
+//Theme Options
+
+add_theme_support('menus');
